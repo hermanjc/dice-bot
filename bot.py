724fd0646562a9debfb9f1c3cf3d74769ca0542e
Jake Herman <tolkwes@gmail.com>
2019-09-08 14:14:48 -0700

    Add modifier support

--- bot.py
+++ bot.py
@@ -12,26 +12,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('/roll'):
-        dice_strs = message.content.split()
-        print(dice_strs)
+    if message.content.startswith('/roll '):
+        dice_str = message.content[message.content.find(' ') + 1:]
 
-        for s in dice_strs[1:]:
-            try:
-                name = message.author.nick if message.author.nick != None else message.author.name
-                # response = message.author.nick + " rolled " + s + ": " + dice.roll_expression(s, long_output=True, special_message=True)
-                response = name + " rolled " + s + ": " + dice.roll_expression(s, long_output=True, special_message=True)
-                # await client.send_message(message.channel, response)
-                await message.channel.send(response)
-            except dice.DiceError as e:
-                print("DiceError:", e.args)
-                # await client.send_message(message.channel, e.args[0] + ". Use `/dice_help` for usage information.")
-                await message.channel.send(str(e.args[0]) + ". Use `/dice_help` for usage information.")
-    elif message.content.startswith('/dice_help'):
-        # await client.send_message(message.channel, "Usage: `/roll ndm` where `n` is the number of dice and `m` is the number of sides.")
+        try:
+            name = message.author.nick if message.author.nick != None else message.author.name
+            response = name + " rolled " + dice_str + ":\n" + '`' + dice.roll_expression(dice_str, long_output=True, special_message=True)[1] + '`'
+            await message.channel.send(response)
+        except dice.DiceError as e:
+            print("DiceError:", e.args)
+            await message.channel.send(str(e.args[0]) + ". Use `/dice_help` for usage information.")
+    elif message.content == '/roll' or message.content == '/dice_help':
         await message.channel.send("Usage: `/roll ndm` where `n` is the number of dice and `m` is the number of sides.")
 
-# Dice Bot Secret
-# client.run('NTYzNDQ0MDI4MzM5NjUwNTcy.XKZeCw.QCFt2_jdjFv_Fr9qFOROQA6MrOs')
-# Dev Bot Secret
-client.run('NjE5MzA2MzUyNjg2MTM3MzU0.XXGUkA.cTXv3Ac50wQ2o5ORXGZVPriVhdk')
+# Put bot token in the quotes below
+# client.run('')
--- dice.py
+++ dice.py
@@ -1,13 +1,9 @@
 import random
 import re
 
-# Dice Regex Expression: (\d+)d(\d+)
-# Dice Regex Expression 2.0: (\d+)d(\d+)|\d
-dice_regex = re.compile(r'(\d+)d(\d+)')
-
-# Decimal Dice Regex Expression: (\d+)d(\d+)\.(\d+)
-
 # Matches all strings: ((\d+)d(\d+)|\d+)(\s*[\+\-\*/]\s*((\d+)d(\d+)|\d+))*
+# Dice Regex Expression: (\d+)d(\d+)|\d+
+dice_regex = re.compile(r'(\d+)d(\d+)|\d+')
 
 class DiceError(RuntimeError):
     def __init__(self, error_string):
@@ -15,57 +11,93 @@ class DiceError(RuntimeError):
 
 def roll_die(sides):
     if(sides > 0):
-        #return random.randrange(1, sides + 1)
         return random.randint(1, sides)
     else:
         raise DiceError("ERROR: Dice must have 1 or more sides")
 
-def roll_expression(expr, long_output=False, special_message=False):
-    # if(dice_regex.match(expr) == None):
-    #     raise DiceError("ERROR: Invalid expression")
-
-    if(re.match(r'((\d+)d(\d+)|\d+)(\s*[\+\-\*/]\s*((\d+)d(\d+)|\d+))*', expr) == None):
+def roll_subexpr(subexpr, long_output=False, special_message=False, parentheses=False, equals=False):
+    if dice_regex.fullmatch(subexpr) == None:
         raise DiceError("ERROR: Invalid expression")
-    
-    num_dice = int(dice_regex.match(expr).group(1))
-    sides = int(dice_regex.match(expr).group(2))
 
     output_str = ""
     total = 0
 
-    for i in range(num_dice):
-        roll = roll_die(sides)
-        total += roll
-
-        if(long_output and num_dice > 1):
-            output_str += str(roll)
-
-            if(i != num_dice - 1):
-                output_str += " + "
-    
-    if(long_output and num_dice > 1):
-        output_str += " = " + str(total)
+    if re.fullmatch(r'\d+', subexpr) != None:
+        total = int(subexpr)
+        output_str += str(subexpr)
     else:
-        output_str += str(total)
+        num_dice = int(dice_regex.match(subexpr).group(1))
+        sides = int(dice_regex.match(subexpr).group(2))
+
+        if long_output and parentheses:
+            output_str += "("
+
+        for i in range(num_dice):
+            roll = roll_die(sides)
+            total += roll
+
+            output_str += str(roll)
+            if long_output and i != num_dice - 1:
+                output_str += " + "    
+        
+        if not long_output:
+            output_str += str(total)
+
+        if long_output and parentheses:
+            output_str += ")"
+        
+        if long_output and num_dice > 1 and equals:
+            output_str += " = " + str(total)
     
-    if(special_message):
-        if(expr == "1d20" and total == 20):
-            output_str += "\n**CRITICAL HIT!**"
-        # elif(expr == "1d20" and total == 1):
-        #     output_str += "\n**CRITICAL FAILURE!**"
-        elif(num_dice > 0 and total == num_dice * sides):
-            output_str += "\n**MAXIMUM DAMAGE!**"
+        if(special_message):
+            if(num_dice == 1 and sides == 20 and total == 20):
+                output_str += "\nCRITICAL HIT!"
+            elif(num_dice == 1 and sides == 20 and total == 1):
+                output_str += "\nCRITICAL FAILURE!"
 
-    return output_str
+    return (total, output_str)
 
-if __name__ == '__main__':
-    print(roll_expression("1d20 + 5", special_message=True))
+def roll_expression(expr, long_output=False, special_message=False):
+    if re.fullmatch(r'((\d+)d(\d+)|\d+)(\s*[\+\-\*/]\s*((\d+)d(\d+)|\d+))*', expr) == None:
+        raise DiceError("ERROR: Invalid expression")
 
-    # for i in range(20):
-    #     print(roll_expression("1d20", special_message=True))
-    # while(1):
-    #     i = input()
-    #     try:
-    #         print(roll_expression(i, special_message=True, long_output=True))
-    #     except DiceError as e:
-    #         print("ERROR: " + e.error_string)
+    total = 0
+    output_str = ""
+    operator = ' ' 
+
+    sub_exprs = expr.split()
+    print(sub_exprs)
+
+    if len(sub_exprs) == 1:
+        return roll_subexpr(sub_exprs[0], long_output=long_output, special_message=special_message, equals=True)
+
+    else:
+        for e in sub_exprs:
+            if(e == '+' or e == '-' or e == '*' or e == '/'):
+                operator = e
+            else:
+                subresult = roll_subexpr(e, long_output=long_output, special_message=False, parentheses=True)
+
+                if(operator == '+'):
+                    total += subresult[0]
+                    output_str += ' + ' + subresult[1]
+                elif(operator == '-'):
+                    total -= subresult[0]
+                    output_str += ' - ' + subresult[1]
+                elif(operator == '*'):
+                    total *= subresult[0]
+                    output_str += ' * ' + subresult[1]
+                elif(operator == '/'):
+                    if e == '0':
+                        raise DiceError("Cannot divide by zero")
+                    total /= subresult[0]
+                    output_str += ' / ' + subresult[1]
+                elif(operator == ' '):
+                    total += subresult[0]
+                    output_str += subresult[1]
+                else:
+                    raise DiceError("Invalid operator")
+                
+        output_str += ' = ' + str(total)
+    
+    return (total, output_str)
