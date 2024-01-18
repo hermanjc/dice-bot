fa333fd89b962b0544b69018007fb130888d10cd
Jake Herman <tolkwes@gmail.com>
2024-01-17 19:48:16 -0800

    Add Intents

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import dice
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
